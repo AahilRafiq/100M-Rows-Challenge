872540f4e837ec9c0b74b3b734162b53c7250ff6
AahilRafiq <aahil.rafiq136@gmail.com>
2025-01-27 19:31:11 +0530

    mmap file reading

--- Solution.java
+++ Solution.java
@@ -1,5 +1,23 @@
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
+
 public class Solution{
     public static void main(String[] args) {
-        
+        try {
+            try (RandomAccessFile file = new RandomAccessFile("measurements.txt", "r")) {
+                FileChannel channel = file.getChannel();
+                MappedByteBuffer buffer = channel.map(MapMode.READ_ONLY, 0, file.length());
+                
+                while(buffer.limit()  > buffer.position()) {
+                    System.out.print((char)buffer.get());
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
