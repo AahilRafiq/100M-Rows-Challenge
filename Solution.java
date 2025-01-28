6d7b6f681a07bab4fe9c1731f5a155f3f1de9b90
AahilRafiq <aahil.rafiq136@gmail.com>
2025-01-28 16:44:51 +0530

    calculate with threads

--- Solution.java
+++ Solution.java
@@ -16,7 +16,7 @@ public class Solution {
         Container.initMaps(numThreads);
 
         try {
-            Thread t = Thread.ofVirtual().unstarted(new Worker(0));
+            Thread t = Thread.ofVirtual().unstarted(new Worker(0,numThreads));
             t.start();
             t.join();
             
@@ -31,9 +31,11 @@ public class Solution {
 class Worker implements Runnable {
 
     private final int threadIdx;
+    private final int numThreads;
 
-    Worker(int threadIdx) {
+    Worker(int threadIdx, int numThreads) {
         this.threadIdx = threadIdx;
+        this.numThreads = numThreads;
     }
 
     @Override
@@ -42,34 +44,58 @@ class Worker implements Runnable {
             FileChannel channel = file.getChannel();
             MappedByteBuffer buffer = channel.map(MapMode.READ_ONLY, 0, file.length());
 
+            int offset = (int)(file.length()/numThreads)*threadIdx;
+            final byte SEMI_COLON_B = (byte) ';';
+            final byte NEWLINE_B = (byte) '\n';
+            
+            // move buffer to offset , and to first line
+            buffer.position(offset);
+            if(offset != 0) {
+                while(buffer.position() < buffer.limit()) {
+                    if(buffer.get() == NEWLINE_B) break; 
+                } 
+            }
+            buffer.get(); // Skip Newline
+            int limit = Math.min(buffer.remaining() , (int)(file.length() / numThreads));
+
             var hashMap = Container.maps.get(threadIdx);
             byte currByte;
             byte[] cityBytes = new byte[300];
             byte[] tempBytes = new byte[300];
             int cityIdx, tempIdx;
-            final byte SEMI_COLON_B = (byte) ';';
-            final byte NEWLINE_B = (byte) '\n';
-
-            while (buffer.position() < buffer.limit()) {
+            
+            // Note : Limit only checked at while loop
+            // Because for case when limit < num of chars in the line , read the whole line
+            while (limit > 0) {
                 cityIdx = 0;
                 tempIdx = 0;
-                while (buffer.position() < buffer.limit() && (currByte = buffer.get()) != SEMI_COLON_B) {
+                while (buffer.hasRemaining()) {
+                    currByte = buffer.get();
+                    limit--;
+                    if(currByte == SEMI_COLON_B) break;
                     cityBytes[cityIdx++] = currByte;
                 }
 
-                buffer.get(); // Ignore ';''
+                if (buffer.hasRemaining()) {
+                    buffer.get(); // Ignore ';'
+                    limit--;
+                }
 
-                while (buffer.position() < buffer.limit() && (currByte = buffer.get()) != NEWLINE_B) {
+                while (buffer.hasRemaining()) {
+                    currByte = buffer.get();
+                    limit--;
+                    if(currByte == NEWLINE_B) break;
                     tempBytes[tempIdx++] = currByte;
                 }
 
-                if (buffer.position() < buffer.limit()) {
+                if (buffer.hasRemaining()) {
                     buffer.get(); // Ignore '\n'
+                    limit--;
                 }
                 Stats.updateStats(
-                        new String(cityBytes, 0, cityIdx),
-                         Double.parseDouble(new String(tempBytes, 0, tempIdx)),
-                         hashMap
+                    new String(cityBytes, 0, cityIdx),
+                    new String(tempBytes, 0, tempIdx),
+                    hashMap
                 );
             }
         } catch (IOException e) {
@@ -104,7 +130,9 @@ class Stats {
         count = 0;
     }
 
-    public static void updateStats(String city, double temp, HashMap<String, Stats> hashMap) {
+    public static void updateStats(String city, String tempStr, HashMap<String, Stats> hashMap) {
+        if(city.isEmpty()) return;
+        Double temp = Double.valueOf(tempStr);
         hashMap.compute(city, (k, stats) -> {
             if (stats == null) {
                 stats = new Stats();
